d8cd1ac1ecf030459a46f16e72d5b790ea0f7815
[username] <[user>
2025-03-25 15:46:50 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Add custom JavaScript here
+
+document.addEventListener("DOMContentLoaded", () => {
+  const text = "I am Adeyemi Ayorinde.";
+  const typingSpeed = 100; // Speed in milliseconds
+  let index = 0;
+
+  const h1Element = document.querySelector("h1");
+  if (h1Element) {
+    const typeEffect = () => {
+      if (index < text.length) {
+        h1Element.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, typingSpeed);
+      }
+    };
+    typeEffect();
+  }
+});
